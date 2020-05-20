6f4399d729f4a7a07ddf6a3b61c8bf1412ddd81f
JoseLRiano <joselriano@yahoo.com>
2020-05-20 01:57:06 -0400

    Updates

---
+++ precache-manifest.659a066c42d993447b803025b21e375b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "67a791aaf78a792822e5",
+    "url": "/LightsOut/static/css/main.3225e646.chunk.css"
+  },
+  {
+    "revision": "67a791aaf78a792822e5",
+    "url": "/LightsOut/static/js/main.67a791aa.chunk.js"
+  },
+  {
+    "revision": "789b1b5576225a7d3213",
+    "url": "/LightsOut/static/js/1.789b1b55.chunk.js"
+  },
+  {
+    "revision": "135e11432d80503c488c",
+    "url": "/LightsOut/static/js/runtime~main.135e1143.js"
+  },
+  {
+    "revision": "e2dfb5871d4d2e6ff5ceb2a66ee5e9d7",
+    "url": "/LightsOut/index.html"
+  }
+];
